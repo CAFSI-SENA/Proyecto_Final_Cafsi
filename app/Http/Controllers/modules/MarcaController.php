01653fd8cd4c3be4e79f3fd4eaba8f3a65aacab7
CAFSI-SENA <51935005+CAFSI-SENA@users.noreply.github.com>
2020-12-30 19:00:40 -0600

    Marca Create

--- app/Http/Controllers/modules/MarcaController.php
+++ app/Http/Controllers/modules/MarcaController.php
@@ -14,4 +14,19 @@ class MarcaController extends Controller
         $estados = Estado::all();
         return view('modules/marcas/index',compact('marcas','estados'));
     }
+
+    public function create(){
+        $marcas = Marca::all();
+        $estados = Estado::all();
+        return view('modules/marcas/create',compact('marcas','estados'));
+    }
+
+    public function store(Request $request){
+        $marcas = Marca::create($request->all());
+        return redirect()->route('marca.index')->with([
+           'message'=>'La marca fue creada con exito :)','type'=>'success'
+        ]);
+    }
+
+
 }
