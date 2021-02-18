94b4eb1d47ecb7ec9676b8b20c80cd37df241cad
CAFSI-SENA <51935005+CAFSI-SENA@users.noreply.github.com>
2021-02-17 20:40:11 -0500

    database

--- resources/views/modules/activos/create.blade.php
+++ resources/views/modules/activos/create.blade.php
@@ -1,14 +1,7 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Crear Activo') }}
-        </h2>
-    </x-slot>
+@extends('layouts.admin.app')
+@section('section')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="container">
+
                     <div class="row">
                         <div class="col-md-12">
                             <form action="{{route('activo.store')}}" method="post" class="row g-3">
@@ -72,8 +65,5 @@
                             </form>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+
+@endsection
