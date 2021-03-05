b8128ec169204b0e37fa18f645c4aa97b012599e
CAFSI-SENA <51935005+CAFSI-SENA@users.noreply.github.com>
2021-03-04 21:06:11 -0500

    asignacion create

--- app/Http/Controllers/modules/AsignacionController.php
+++ app/Http/Controllers/modules/AsignacionController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\modules;
 use App\Http\Controllers\Controller;
 use App\Models\Asignacion;
 use Illuminate\Http\Request;
+use App\Models\Activo;
+use App\Models\Funcionario;
 
 class AsignacionController extends Controller
 {
@@ -14,7 +16,19 @@ class AsignacionController extends Controller
     }
 
     public function create(){
+
+        $funcionario = [];
+        $activo = [];
+
+        if (@$_GET['numero_serie']) {
+            $activo = Activo::where('numero_serie',$_GET['numero_serie'])->first();
+
+        }
+        if (@$_GET['identificacion']) {
+            $funcionario = Funcionario::where('identificacion',$_GET['identificacion'])->first();
+        }
+
         $asignaciones = Asignacion::all();
-        return view('modules/asignaciones/create', compact('asignaciones'));
+        return view('modules/asignaciones/create', compact('asignaciones','funcionario','activo'));
     }
 }
--- resources/views/modules/asignaciones/create.blade.php
+++ resources/views/modules/asignaciones/create.blade.php
@@ -3,50 +3,52 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            <form action="" class="row g-3">
+            <form action="" class="row g-3" method="get">
+                <input type="hidden" name="identificacion" value="{{ @$_GET['identificacion'] }}">
                 <div class="col-md-2 mt-4">
                     <label for="" class="form-label">Serie</label>
-                    <input type="text" class="form-control">
+                    <input type="text" name="numero_serie" class="form-control" value="{{ @$_GET['numero_serie'] }}">
                 </div>
                 <div class="col-md-1 mt-5">
                     <button class="btn btn-outline-secondary" type="submit"><img src="/search.svg" alt=""></button>
                 </div>
                 <div class="col-md-2 mt-4">
                     <label for="" class="form-label">Categoría</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" value="{{ @$activo->categoria_id }}">
                 </div>
                 <div class="col-md-2 mt-4">
                     <label for="">Tipo</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" value="{{ @$activo->tipo_activo_id }}">
                 </div>
                 <div class="col-md-2 mt-4">
                     <label for="">Marca</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" value="{{ @$activo->marca_id }}">
                 </div>
                 <div class="col-md-2 mt-4">
                     <label for="">Modelo</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" value="{{ @$activo->modelo }}">
                 </div>
             </form>
             <form action="" class="row g-3">
+                <input type="hidden" name="numero_serie" value="{{ @$_GET['numero_serie'] }}">
                 <div class="col-md-2 mt-4">
                     <label for="">No. Documento</label>
-                    <input type="number" class="form-control">
+                    <input type="number" value="{{  @$_GET['identificacion'] }}" class="form-control" name="identificacion">
                 </div>
                 <div class="col-md-1 mt-5">
                     <button type="submit" class="btn btn-outline-secondary"><img src="/search.svg" alt=""></button>
                 </div>
                 <div class="col-md-4 mt-4">
                     <label for="">Funcionario Prestamo</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" value="{{ @$funcionario->nombres }}">
                 </div>
                 <div class="col-md-2 mt-4">
                     <label for="">Celular</label>
-                    <input type="number" class="form-control">
+                    <input type="number" class="form-control" value="{{ @$funcionario->celular }}">
                 </div>
                 <div class="col-md-3 mt-4">
                     <label for="">Área</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" value="{{ @$funcionario->area_id }}">
                 </div>
 
             </form>
