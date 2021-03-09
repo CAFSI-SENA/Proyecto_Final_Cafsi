4c70ddb6f2af8336dad203814a85083b2bf121cb
CAFSI-SENA <51935005+CAFSI-SENA@users.noreply.github.com>
2021-03-08 22:05:25 -0500

    bajas edit

--- resources/views/modules/bajas/edit.blade.php
+++ resources/views/modules/bajas/edit.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
-                <form action="">
+            <div class="col-md-6 offset-4">
+                <form action="{{route('baja.store')}}">
                     <table class="table">
                         <tbody>
                         <th>Fecha de Baja</th>
@@ -12,8 +12,25 @@
                         </tbody>
                         <tbody>
                         <th>Tipo de Baja</th>
+                        <td>
+                            <select name="tipo_baja_id" id="" class="form-select">
+                                @foreach($tipos as $tipo)
+                                    <option value="{{$tipo->tipo_baja_id}}"
+                                    @if($bajas->tipo_baja_id == $tipo->id)
+                                        selected
+                                    @endif
+                                    >{{$tipo->tipo}}</option>
+                                @endforeach
+                            </select>
+                        </td>
+                        </tbody>
+                        <tbody>
+                        <th>Serie</th>
+                        <td></td>
                         </tbody>
                     </table>
+                    <a href="{{route('baja.index')}}" class="btn btn-outline-dark">Cancelar</a>
+                    <button type="submit" class="btn btn-outline-primary">Guardar</button>
                 </form>
             </div>
         </div>
