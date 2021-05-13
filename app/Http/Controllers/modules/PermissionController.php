6b50f7dec41fbdfa1cb216d38810b5725b5d6da6
CAFSI-SENA <51935005+CAFSI-SENA@users.noreply.github.com>
2021-05-13 13:30:11 -0500

    Roles y Permisos

--- app/Http/Controllers/modules/PermissionController.php
+++ app/Http/Controllers/modules/PermissionController.php
@@ -4,15 +4,34 @@ namespace App\Http\Controllers\modules;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
     const PERMISSIONS = [
         'show' => 'admin-permissions-show',
     ];
-
+/*
     public function __construct()
     {
         $this->middleware('permission:'.self::PERMISSIONS['show'])->only(['index','show']);
+    }
+*/
+    public function index()
+    {
+        $permissions = Permission::orderBy('name')->paginate();
+
+        return view('modules/permisos/index',
+            [
+                'rows' => $permissions,
+            ]);
+    }
+
+    public function show(Permission $permission)
+    {
+        return view('modules/permisos/show',
+            [
+                'row' => $permission->load('roles', 'users'),
+            ]);
     }
 }
--- app/Http/Controllers/modules/RoleController.php
+++ app/Http/Controllers/modules/RoleController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\modules;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
@@ -20,5 +22,73 @@ class RoleController extends Controller
         $this->middleware('permission:'.self::PERMISSIONS['show'])->only(['index','show']);
         $this->middleware('permission:'.self::PERMISSIONS['edit'])->only(['edit','update']);
         $this->middleware('permission:'.self::PERMISSIONS['delete'])->only(['destroy']);
+    }
+
+    public function index()
+    {
+        $rows = Rol::all();
+        return view('modules/roles/index', [
+            'rows' => $rows,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('modules/roles/create', [
+            'row' => new Role(),
+            'permissions' => Permission::all(),
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $roles = Role::create($request->all());
+        $roles->permissions()->sync($request->permission);
+        return redirect()->route('rol.index');
+    }
+
+    public function show(Role $role)
+    {
+        return view('modules/roles/show', [
+            'row' => $role->load('permissions','users')
+        ]);
+    }
+
+    public function edit(Role $role)
+    {
+        return view('modules/roles/edit', [
+            'row' => $role,
+            'permissions' => Permission::all(),
+        ]);
+    }
+
+    public function update(Request $request, Role $role)
+    {
+        $role->update($request->all());
+        $role->permissions()->sync($request->permission);
+        return redirect()->route('rol.show', $role->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $role = Role::find($id)->delete();
+        return redirect()->route('rol.index');
     }
 }
