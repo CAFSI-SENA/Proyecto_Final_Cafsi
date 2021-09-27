fb27438db763b6be58a703d244d9893959357640
Spartan2448 <71468930+Spartan2448@users.noreply.github.com>
2021-09-27 14:34:17 -0500

    ajuste para no visulziar gif engranajey opciones al desplegar lista nombre de usuario, nav bar

--- resources/views/layouts/admin/componentes/navbar.blade.php
+++ resources/views/layouts/admin/componentes/navbar.blade.php
@@ -38,9 +38,9 @@
                 </button>
                 <div class="dropdown-menu dropdown-menu-end">
                     <!-- item-->
-                    <a class="dropdown-item" href="{{ route('profile.show') }}"><i class="bx bx-user font-size-16 align-middle me-1"></i> <span key="t-profile">Profile</span></a>
-                    <a class="dropdown-item d-block" href="#"><span class="badge bg-success float-end">11</span><i class="bx bx-wrench font-size-16 align-middle me-1"></i> <span key="t-settings">Settings</span></a>
-                    <div class="dropdown-divider"></div>
+                   <!-- <a class="dropdown-item" href="{{ route('profile.show') }}"><i class="bx bx-user font-size-16 align-middle me-1"></i> <span key="t-profile">Profile</span></a>-->
+                    <!--<a class="dropdown-item d-block" href="#"><span class="badge bg-success float-end">11</span><i class="bx bx-wrench font-size-16 align-middle me-1"></i> <span key="t-settings">Settings</span></a>-->
+                   <!-- <div class="dropdown-divider"></div>-->
 
                     <a class="dropdown-item text-danger" href="javascript:document.getElementById('logout').submit()"
                     ><i class="bx bx-power-off font-size-16 align-middle me-1 text-danger"></i> <span key="t-logout">Salir</span></a>
@@ -51,11 +51,11 @@
                 </div>
             </div>
 
-            <div class="dropdown d-inline-block">
+          <!--gif engranaje  <div class="dropdown d-inline-block">
                 <button type="button" class="btn header-item noti-icon right-bar-toggle waves-effect">
                     <i class="bx bx-cog bx-spin"></i>
                 </button>
-            </div>
+            </div>-->
 
         </div>
     </div>
